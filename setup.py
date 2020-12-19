1d17406845ca360203c3a1e0bdec442b30549a3c
Allison Casey <alliecasey21@gmail.com>
2020-12-19 12:32:02 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+requires = [
+    "Sphinx >= 3.3",
+    "hy >= 0.19",
+    "sphinxcontrib-hydomain >= 0.1"
+]
+
+def readme():
+    try:
+        return Path("README.rst").read_text()
+    except IOError:
+        return None
+
+setup(
+    name='sphinxcontrib-hydomain',
+    version='0.1.0',
+    url='https://github.com/allison-casey/sphinxcontrib-hydomain.git',
+    license='BSD',
+    author='Allison Casey',
+    author_email='alliecasey21@gmail.com',
+    description='Sphinx domain for documenting Hy programs',
+    long_description=readme(),
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Documentation',
+        'Topic :: Utilities',
+    ],
+    platforms='any',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=requires,
+    namespace_packages=['sphinxcontrib'],
+)
